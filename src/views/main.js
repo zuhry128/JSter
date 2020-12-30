1da349e1d0d1966bd05db420350760fc58c25f3c
Azzuhry <muhammadazzuhry03@gmail.com>
2020-12-30 11:29:47 +0700

    this works but i dunno why it didnt in other computer

--- src/views/main.js
+++ src/views/main.js
@@ -1,6 +1,8 @@
 import DataSource from '../data/data-source.js';
+import '../components/SearchButton.js';
 
 const main = () => {
+    const searchElement = document.querySelector("search-button");
 
 };
 
